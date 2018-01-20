d724da31676dd3b491e79907b2de7a43c38154c5
Volski <volski.alex@gmail.com>
2018-01-20 16:11:07 +0200

    Initial Commit

---
+++ testClass/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  testClass
+//
+//  Created by MyMac on 20/01/2018.
+//  Copyright © 2018 MyMac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
